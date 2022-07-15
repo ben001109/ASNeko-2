f5db6ee12db6e0afcbb69c7fb9c325123250f94a
死神(ben001109) <a0903932792@gmail.com>
2022-07-15 13:45:07 +0800

    added a package to write some thing with the tweaker
    I added a package for the mod's main core called com.asneko.tweak

---
+++ mod_tweaks/src/main/java/com/asneko/tweak/asnekotewak.java
@@ -0,0 +1,14 @@
+package com.asneko.tweak;
+
+public class asnekotewak {
+
+	public asnekotewak() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
