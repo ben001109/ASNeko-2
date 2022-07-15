cb28143437e7ce7bd1be66fc1cbb5adb6451c711
死神(ben001109) <a0903932792@gmail.com>
2022-07-15 14:44:17 +0800

    Update asnekotewak.java

--- mod_tweaks/src/main/java/com/asneko/tweak/asnekotewak.java
+++ mod_tweaks/src/main/java/com/asneko/tweak/asnekotewak.java
@@ -3,12 +3,11 @@ package com.asneko.tweak;
 public class asnekotewak {
 
 	public asnekotewak() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
