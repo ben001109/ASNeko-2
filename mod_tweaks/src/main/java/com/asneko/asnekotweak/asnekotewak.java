655df8cf0134599b0281ece1dd8c913be3ba1612
死神(ben001109) <a0903932792@gmail.com>
2022-07-20 01:51:41 +0800

    half work

---
+++ mod_tweaks/src/main/java/com/asneko/asnekotweak/asnekotewak.java
@@ -0,0 +1,21 @@
+package com.asneko.asnekotweak;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("asnekotweak")
+public class asnekotewak {
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MOD_ID = "asnekotweak";
+
+	public asnekotewak() {		
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
---
+++ mod_tweaks/src/main/java/com/asneko/asnekotweaker/asnekotewak.java
@@ -0,0 +1,20 @@
+package com.asneko.tweak;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("asnekotweak")
+public class asnekotewak {
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	public asnekotewak() {		
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this));
+	}
+}
